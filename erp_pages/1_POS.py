78a2bed2d35a7af44d1ce0cc77394320f1b9272c
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-07-27 10:17:55 +0630

    Fix indentation for tax_rate session state check

--- erp_pages/1_POS.py
+++ erp_pages/1_POS.py
@@ -270,7 +270,7 @@ def run():
 
         st.write("DEBUG TAX FROM DB =", tax_setting)
 
-      if "tax_rate" in st.session_state:
+    if "tax_rate" in st.session_state:
         del st.session_state["tax_rate"]
 
         st.session_state.tax_rate = float(tax_setting)
