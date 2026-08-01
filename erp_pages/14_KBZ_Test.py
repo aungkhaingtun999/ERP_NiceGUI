6cb05a8bd9ef401f3c6bbcf63bfec55d8e29a6e6
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-08-01 14:31:02 +0630

    Add files via upload

---
+++ erp_pages/14_KBZ_Test.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+from erp_core.payments.kbz_qr_analyzer import KBZQRAnalyzer
+
+
+st.title(
+    "🔍 KBZ Pay QR Analyzer"
+)
+
+
+qr_data = st.text_area(
+    "Paste KBZ QR Data"
+)
+
+
+
+if st.button(
+    "Analyze"
+):
+
+    result = KBZQRAnalyzer.analyze(
+        qr_data
+    )
+
+
+    st.json(
+        result
+    )
