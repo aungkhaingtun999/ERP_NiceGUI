77147daa49c7f2af20a68c90281fe2dae2da9f9e
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-08-02 20:50:51 +0630

    Update __init__.py

--- erp_components/zxing_barcode/__init__.py
+++ erp_components/zxing_barcode/__init__.py
@@ -1,41 +1,40 @@
-# ==============================================================================
-# ZXING BARCODE STREAMLIT COMPONENT
-# ==============================================================================
-
-
 import os
 
 import streamlit.components.v1 as components
 
 
-
-_RELEASE = False
+# Production / Streamlit Cloud
+_RELEASE = True
 
 
 
 if _RELEASE:
 
     _component_func = components.declare_component(
+
         "zxing_barcode",
+
         path=os.path.join(
             os.path.dirname(__file__),
             "frontend"
         )
+
     )
 
 
 else:
 
     _component_func = components.declare_component(
+
         "zxing_barcode",
+
         url="http://localhost:3001"
+
     )
 
 
 
-def zxing_scanner(
-    key=None
-):
+def zxing_scanner(key=None):
 
     return _component_func(
         key=key,
