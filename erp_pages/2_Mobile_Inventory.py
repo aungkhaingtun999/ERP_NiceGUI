4a48ab4e8619106c3936d32b53d9fd3d9707fb32
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-08-02 18:31:40 +0630

    Replace barcode_input with get_barcode import

--- erp_pages/2_Mobile_Inventory.py
+++ erp_pages/2_Mobile_Inventory.py
@@ -5,9 +5,7 @@
 # ==============================================================================
 
 import streamlit as st
-
-from erp_pages.inventory.scanner import (
-    barcode_input
+from erp_pages.inventory.scanner import get_barcode
 )
 from erp_pages.inventory.product_search import (
     search_product,
