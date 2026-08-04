d19cb6c6f5109aafdb22a88e7ee6f6fb4007f772
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-08-04 23:07:17 +0630

    Refactor ZXing scanner path and add file existence checks
    Updated paths for ZXing scanner component and added checks for frontend files.

--- erp_pages/inventory/zxing_scanner.py
+++ erp_pages/inventory/zxing_scanner.py
@@ -2,25 +2,56 @@ import os
 import streamlit.components.v1 as components
 
 
-CURRENT_DIR = os.path.dirname(
-    os.path.abspath(__file__)
-)
+CURRENT = os.path.abspath(__file__)
 
-
-COMPONENT_PATH = os.path.abspath(
+PROJECT_ROOT = os.path.abspath(
     os.path.join(
-        CURRENT_DIR,
-        "../../erp_components/zxing_scanner/frontend"
+        os.path.dirname(__file__),
+        "../.."
     )
 )
 
 
-print("ZXING PATH:", COMPONENT_PATH)
+FRONTEND = os.path.join(
+    PROJECT_ROOT,
+    "erp_components",
+    "zxing_scanner",
+    "frontend"
+)
+
+
+print("==========================")
+print("CURRENT FILE:")
+print(CURRENT)
+
+print("PROJECT ROOT:")
+print(PROJECT_ROOT)
+
+print("FRONTEND PATH:")
+print(FRONTEND)
+
+print("FRONTEND EXISTS:")
+print(os.path.exists(FRONTEND))
+
+print("INDEX EXISTS:")
+print(
+    os.path.exists(
+        os.path.join(FRONTEND, "index.html")
+    )
+)
+
+print("MAIN EXISTS:")
+print(
+    os.path.exists(
+        os.path.join(FRONTEND, "main.js")
+    )
+)
+print("==========================")
 
 
 zxing_component = components.declare_component(
     "zxing_scanner",
-    path=COMPONENT_PATH
+    path=FRONTEND
 )
 
 
