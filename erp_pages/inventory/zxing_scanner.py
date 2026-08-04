a2d982fc6f2c3d5197e1e0087b56f80bf852f24b
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-08-04 20:27:14 +0630

    Update zxing_scanner.py

--- erp_pages/inventory/zxing_scanner.py
+++ erp_pages/inventory/zxing_scanner.py
@@ -1,79 +1,24 @@
+import os
 import streamlit.components.v1 as components
+
+
+_COMPONENT_PATH = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        "../../erp_components/zxing_scanner/frontend"
+    )
+)
+
+
+zxing_component = components.declare_component(
+    "zxing_scanner",
+    path=_COMPONENT_PATH
+)
 
 
 def zxing_scanner():
 
-    html = """
-    <html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://unpkg.com/@zxing/library@latest"></script>
-    </head>
-
-    <body style="text-align:center">
-
-    <video id="video"
-        style="
-        width:100%;
-        max-width:400px;
-        border-radius:10px;
-        ">
-    </video>
-
-    <p id="result">
-        📷 Waiting scan...
-    </p>
-
-
-    <script>
-
-    const reader =
-        new ZXing.BrowserMultiFormatReader();
-
-    const video =
-        document.getElementById("video");
-
-    const result =
-        document.getElementById("result");
-
-
-    reader.decodeFromVideoDevice(
-        null,
-        video,
-        (code, err)=>{
-
-            if(code){
-
-                result.innerHTML =
-                    "✅ " + code.text;
-
-
-                window.parent.postMessage(
-                {
-                    type:
-                    "streamlit:setComponentValue",
-
-                    value:
-                    code.text
-                },
-                "*"
-                );
-
-
-                reader.reset();
-
-            }
-
-        }
-    );
-
-    </script>
-
-    </body>
-    </html>
-    """
-
-    return components.html(
-        html,
-        height=450
+    return zxing_component(
+        key="barcode_scanner",
+        default=""
     )
