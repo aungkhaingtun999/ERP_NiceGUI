fe6109bee84ebd142aa384ae64c85298885385f5
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-08-07 22:11:10 +0630

    Simplify execution of 2_Inventory.py
    Removed unnecessary main check and simplified execution.

--- erp_pages/2_Inventory.py
+++ erp_pages/2_Inventory.py
@@ -3,10 +3,8 @@
 # ERP ENTERPRISE INVENTORY ENTRY
 # ==============================================================================
 
+
 from erp_pages.inventory.page import run
 
 
-if __name__ == "__main__":
-    run()
-else:
-    run()
+run()
